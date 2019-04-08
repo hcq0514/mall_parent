71018f87a559bec97c385fff03a5a641161c06a1
hcq <hcq0514@163.com>
2019-04-08 15:10:33 +0800

    item-service

---
+++ mall_item/mall_item_service/src/main/java/com/mall/item/service/ItemServiceApplication.java
@@ -0,0 +1,18 @@
+package com.mall.item.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class);
+    }
+
+
+
+}
