aeac095f9c8e46f1135898e1e5bc4cb8473c00cc
hcq <hcq0514@163.com>
2019-04-26 14:34:28 +0800

    thymeleaf client

---
+++ mall_goods_web/src/main/java/com/mall/goods/web/client/GoodsClient.java
@@ -0,0 +1,8 @@
+package com.mall.goods.web.client;
+
+import com.mall.item.api.GoodsApi;
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient("item-service")
+public interface GoodsClient extends GoodsApi {
+}
